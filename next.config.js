a3932d72322de321c807bcfd017521903bc2584e
MrGoblings <emomomchev@gmail.com>
2024-01-19 15:27:14 +0200

    fix: added url for pfp in github

--- next.config.js
+++ next.config.js
@@ -5,8 +5,10 @@ const nextConfig = {
           {
             protocol: 'https',
             hostname: 'lh3.googleusercontent.com',
-            port: '',
-            pathname: '/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'avatars.githubusercontent.com',
           },
         ],
       },
