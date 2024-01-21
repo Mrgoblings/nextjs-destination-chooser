7277e4f5176b532bb8ff0d606bf996e8a6551162
MrGoblings <emomomchev@gmail.com>
2024-01-22 01:00:05 +0200

    add: smth for the auth api

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,20 @@
 import NextAuth from "next-auth"
 import { authOptions } from "@/lib/auth"
 
+/**
+ * @swagger
+ * /api/auth:
+ *   get:
+ *     summary: Get authentication handler
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *   post:
+ *     summary: Post authentication handler
+ *     responses:
+ *       200:
+ *         description: Successful response
+ */
 const handler = NextAuth(authOptions);
 
 export {handler as GET, handler as POST};
